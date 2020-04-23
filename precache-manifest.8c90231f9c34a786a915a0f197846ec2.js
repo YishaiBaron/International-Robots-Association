60ac30d6828e9129e1b751971dcfccdb2c2ce39b
Yishai Baron <yishai.baron@gmail.com>
2020-04-23 17:24:33 +0300

    Updates

---
+++ precache-manifest.8c90231f9c34a786a915a0f197846ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5a1b8b777d905a8d0ea5aeb39d3534",
+    "url": "/International-Robots-Association/index.html"
+  },
+  {
+    "revision": "d8f34e0b8b186631ed6c",
+    "url": "/International-Robots-Association/static/css/main.2b50a5dd.chunk.css"
+  },
+  {
+    "revision": "2f72884081d7046fc130",
+    "url": "/International-Robots-Association/static/js/2.61452a99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/International-Robots-Association/static/js/2.61452a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f34e0b8b186631ed6c",
+    "url": "/International-Robots-Association/static/js/main.24326c47.chunk.js"
+  },
+  {
+    "revision": "cba16d0a8b4922e97626",
+    "url": "/International-Robots-Association/static/js/runtime-main.ad2b04f1.js"
+  }
+]);
